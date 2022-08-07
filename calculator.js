84c143194d65ea56296cdc49264b6d2806cef8e5
Jason <jasonhuggins11@outlook.com>
2022-08-07 23:36:04 +0100

    Add function to call given operation

--- calculator.js
+++ calculator.js
@@ -13,3 +13,20 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "add":
+            add(num1, num2);
+            break;
+        case "subtract":
+            subtract(num1, num2);
+            break;
+        case "multiply":
+            multiply(num1, num2);
+            break;
+        case "divide":
+            divide(num1, num2);
+            break;
+    }
+}
